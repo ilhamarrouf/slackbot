8def5666c2db41cc676b4caa99f09718d3666143
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2018-01-28 10:51:13 +0700

    add response

--- run.py
+++ run.py
@@ -4,13 +4,23 @@ from slackbot.bot import respond_to
 import random
 import re
 
+@listen_to('assalamualaikum', re.IGNORECASE)
+def assalamualaikum(message):
+    message.reply('waalaikumsalam', in_thread=True)
+
+    message.react('blush')
+
 @listen_to('afk', re.IGNORECASE)
 def afk(message):
     message.reply(random.choice(['Hayo mau kemana???',
                                  'Udah ijin sama bosque???',
-                                 'Jangan lama-lama nanti aku kangen']))
+                                 'Jangan lama-lama nanti bosque kangen',
+                                 'Kerjaan sudah kelar belom???']))
 
-    message.react('kissing_heart')
+    message.react(random.choice(['ghost',
+                                 'thinking_face',
+                                 'smirk',
+                                 'yum']))
 
 @listen_to('wkw', re.IGNORECASE)
 def wkw(message):
@@ -20,7 +30,9 @@ def wkw(message):
 
 @listen_to('off', re.IGNORECASE)
 def off(message):
-    message.reply('Semoga harimu menyenangkan...')
+    message.reply(random.choice(['Semoga harimu menyenangkan...',
+                                 'Jangan lupa isi checkout ya',
+                                 'Kerjaan sudah kelar belom???']))
 
     message.react('hugging_face')
 
@@ -29,6 +41,18 @@ def senin(message):
     message.reply('Hari masih senin ayo semangat. Jangan kasih kendor')
 
     message.react('hugging_face')
+
+@listen_to('bos', re.IGNORECASE)
+def bos(message):
+    message.reply('Disini aku bosnya :p')
+
+    message.react('ok_hand')
+
+@listen_to('maaf', re.IGNORECASE)
+def maaf(message):
+    message.reply('Karena bosque baik, maka bosque maafin')
+
+    message.react('kissing_heart')
 
 def main():
     bot = Bot()
